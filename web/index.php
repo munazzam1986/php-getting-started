de9729cc517176ba33f27ffa1a2919b9d4867305
munazzam1986 <munazzamm@gmail.com>
2018-09-18 18:11:59 -0400

    adding first files

--- web/index.php
+++ web/index.php
@@ -13,17 +13,12 @@ $app->register(new Silex\Provider\MonologServiceProvider(), array(
 // Register view rendering
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/views',
-));
+));,array('title')
 
 // Our web handlers
 
 $app->get('/', function() use($app) {
   $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig');
-});
+  return $app['twig']->render('index.twig'});
 
-$app->get('/cowsay', function() use($app) {
-    $app['monolog']->addDebug('cowsay');
-    return "<pre>".\Cowsayphp\Cow::say("Cool beans")."</pre>";
-});
-$app->run();
+  $app->run();
