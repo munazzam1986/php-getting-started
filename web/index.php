de478df1c1d1763204f9584084f3df50ae0612bf
munazzam1986 <munazzamm@gmail.com>
2018-09-18 18:35:29 -0400

    fixing changes

--- web/index.php
+++ web/index.php
@@ -13,12 +13,13 @@ $app->register(new Silex\Provider\MonologServiceProvider(), array(
 // Register view rendering
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/views',
-));,array('title')
+));
 
 // Our web handlers
 
 $app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig'});
+    $app['monolog']->addDebug('logging output.');
+    return $app['twig']->render('index.twig');
+});
 
-  $app->run();
+$app->run();
